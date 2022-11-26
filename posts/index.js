d4d810352d2d278e8286b6a68f20c348eaa9d4e7
Hemant soni <sonihemant4741@gmail.com>
2022-11-26 13:21:26 +0530

    TEST: routes for post service

--- posts/index.js
+++ posts/index.js
@@ -1,19 +1,27 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const { rondomBytes, randomBytes } = require("crypto");
 
 const app = express();
+app.use(bodyParser.json());
 
 const posts = {};
 
+app.get("/posts", (req, res) => {
+  res.send(posts);
+});
 
+app.post("/posts", (req, res) => {
+  const { title } = req.body;
+  const id = randomBytes(4).toString("hex");
 
-app.get("/posts", (req,res) => {
- res.send(posts);
-})
-
-app.post("/posts", (req,res) => {
-
-})
+  posts[id] = {
+    id,
+    title,
+  };
+  res.status(201).send(post[id]);
+});
 
 app.listen(4000, () => {
-    console.log(`listen to port 4000`)
-})
+  console.log(`listen to port 4000`);
+});
