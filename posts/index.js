726a77fbbfe9dd5f13344961193e5ce6a62d9608
Hemant soni <sonihemant4741@gmail.com>
2022-11-26 13:11:23 +0530

    ADD: post service routes setup

---
+++ posts/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+const app = express();
+
+const posts = {};
+
+
+
+app.get("/posts", (req,res) => {
+ res.send(posts);
+})
+
+app.post("/posts", (req,res) => {
+
+})
+
+app.listen(4000, () => {
+    console.log(`listen to port 4000`)
+})
