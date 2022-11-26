5c37ea9265e541d873f12e7585b4b31ee016d353
Hemant soni <sonihemant4741@gmail.com>
2022-11-26 20:06:09 +0530

    NEW: routes for comments

---
+++ comments/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const { randomBytes } = require("crypto");
+
+const app = express();
+app.use(bodyParser.json());
+
+const commentsByPostId = {};
+
+app.get("/posts/:id/comments", (req, res) => {
+  res.send(commentsByPostId[req.params.id] || []);
+});
+
+app.post("/posts/:id/comments", (req, res) => {
+  const { content } = req.body;
+  const commentId = randomBytes(4).toString("hex");
+
+  const comments = commentsByPostId[req.params.id] || [];
+
+  comments.push({ id: commentId, content });
+  commentsByPostId[req.params.id] = comments;
+
+  res.status(201).send(posts[id]);
+});
+
+app.listen(5000, () => {
+  console.log(`listen to port 5000`);
+});
