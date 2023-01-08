3f486fb47f31d459db0248c7f8f5ab592a3d708f
Hemant soni <sonihemant4741@gmail.com>
2023-01-08 12:18:04 +0530

    Docke file include

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -4,8 +4,7 @@ function CommentList({ comments }) {
 
   const renderedComments = comments.map(comment => {
       return <li key={comment.id}>{comment.content}</li>
-  })
-
+  });
   return <ul>
       {renderedComments}
   </ul>
